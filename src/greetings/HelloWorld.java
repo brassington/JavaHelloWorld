ab885ae55be38a0fb4817d667772451c0ac7974f
J. Andrew Brassington <jabbrass@zoho.com>
2014-05-06 20:47:36 -0500

    Played more with primitive types in Java

--- src/greetings/HelloWorld.java
+++ src/greetings/HelloWorld.java
@@ -1,8 +1,24 @@
 package greetings;
 
 public class HelloWorld {
+
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        int number = 3;
+        number += 2;
+        String name = "Joe";
+        System.out.println(number);
+        System.out.println(name);
+
+        String firstName = "John";
+        String fullName = firstName + " Sonmez";
+        System.out.println(fullName.indexOf('z'));
+
+        int[] numbers = new int[10];
+        numbers[0] = 5;
+        numbers[1] = 8;
+
+        System.out.print(numbers[2]);
     }
 }
